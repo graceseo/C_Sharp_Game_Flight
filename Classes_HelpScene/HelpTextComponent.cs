303f95b64041ec8bd252c8dbf3b53998296450a2
graceseo <dbmaster1004@gmail.com>
2018-12-08 15:23:03 -0500

    Finished everything

--- Classes_HelpScene/HelpTextComponent.cs
+++ Classes_HelpScene/HelpTextComponent.cs
@@ -7,17 +7,37 @@ namespace GSeoFinalProject
     class HelpTextComponent : DrawableGameComponent
     {
         Game1 game;
-        Texture2D texture;
+        Texture2D descripsionTexture;
+        Texture2D keboardTexture;
+        Texture2D currentHelpImage;
 
         public HelpTextComponent(Game game) : base(game)
         {
             this.game = game as Game1;
         }
-
-
         public override void Initialize()
         {
             base.Initialize();
+        }
+        protected override void LoadContent()
+        {
+            descripsionTexture = game.Content.Load<Texture2D>("Images/helpFirst");
+            keboardTexture = game.Content.Load<Texture2D>("Images/keyboard");
+            currentHelpImage = descripsionTexture;
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                currentHelpImage = keboardTexture;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                currentHelpImage = descripsionTexture;
+            }
+            base.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime)
@@ -25,17 +45,10 @@ namespace GSeoFinalProject
             SpriteBatch spriteBatch = game.Services.GetService<SpriteBatch>();
 
             spriteBatch.Begin();
-            spriteBatch.Draw(texture, Vector2.Zero, Color.White);
+            spriteBatch.Draw(currentHelpImage, Vector2.Zero, Color.White);
             spriteBatch.End();
 
             base.Draw(gameTime);
-        }
-
-
-        protected override void LoadContent()
-        {
-           // texture = game.Content.Load<Texture2D>("Images/helpImage");
-            base.LoadContent();
         }
     }
 }
